6de9e75b8a982ff4f8101144cbb8bb7c51e00898
Mary Mbugua <marymbugua.nm@gmail.com>
2018-05-21 15:29:56 +0300

    created base.html

--- insta/views.py
+++ insta/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 # Create your views here.
 def landing(request):
 
-    return render(request,'navbar.html')
+    return render(request,'base.html')
