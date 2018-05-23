a464b0a6f97e511b527874157ff76901fc7a2ef0
Mary Mbugua <marymbugua.nm@gmail.com>
2018-05-23 16:22:24 +0300

    posting image and displaying image on the feed page

--- insta/forms.py
+++ insta/forms.py
@@ -1,2 +1,13 @@
 from django import forms
-from .models import Profile,Photo
+from .models import Profile,Image
+
+class NewPhotoForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['likes','profile']
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user','following']
+
--- insta/views.py
+++ insta/views.py
@@ -1,22 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from .forms import NewPhotoForm,NewProfileForm
+from .models import Profile,Image
 # Create your views here.
 def landing(request):
+    images = Image.objects.all()
+    
 
-    return render(request,'index.html')
+    return render(request,'index.html',{"images":images})
 
 @login_required(login_url='/accounts/login/')
 def profile(request):
-
-    return render(request,'profile.html')
+    current_user = request.user
+    profile1=Profile.objects.get(id=request.user.id)
+    form = NewPhotoForm(request.POST,request.FILES)
+    if request.method == 'POST': 
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.profile = profile1
+            image.save()
+    else:
+        form = NewPhotoForm()
+    return render(request,'profile.html',{"form":form})
 
 @login_required(login_url='/accounts/login/')
 def explore(request):
 
     return render(request,'search.html')
-    
+
 @login_required(login_url='/accounts/login/')
 def edit(request):
+    current_user = request.user
+    profile=Profile.objects.get(id=request.user.id)
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST,request.FILES,instance=profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+    else:
+        form = NewProfileForm()
 
-    return render(request,'edit.html')
+    return render(request,'edit.html',{"form": form})
