32d5f964a3f1bb2d2ac241b043722f5c8df923b9
Mary Mbugua <marymbugua.nm@gmail.com>
2018-05-23 09:29:46 +0300

    put settings to upload images

--- insta/models.py
+++ insta/models.py
@@ -10,7 +10,7 @@ from tinymce.models import HTMLField
 class ProfileManager(models.Manager):
     use_for_related_fields = True
 
-   def all(self):
+    def all(self):
         qs = self.get_queryset().all()
         try:
             if self.instance:
@@ -22,38 +22,38 @@ class ProfileManager(models.Manager):
 
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='avatar/', blank=True)
-    bio = models.TextField()
+    bio = HTMLField()
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     first_name = models.CharField(max_length=30, null=True)
     last_name = models.CharField(max_length=30, null=True)
     following = models.ManyToManyField(User, blank=True, related_name='followed_by')
 
-   objects = ProfileManager()
+    objects = ProfileManager()
 
-   def get_following(self):
+    def get_following(self):
         users = self.following.all()
         return users.exclude(username=self.user.username)
 
-   def __str__(self):
+    def __str__(self):
         return self.first_name
 
-   @classmethod
+    @classmethod
     def get_all_profiles(cls):
         profile = Profile.objects.all()
         return profile
 
-   @classmethod
+    @classmethod
     def get_searched_profile(cls, search_term):
         profiles = cls.objects.filter(first_name__icontains=search_term)
         return profiles
 
-   def save_profile(self):
+    def save_profile(self):
         self.save()
 
-   def delete_profile(self):
+    def delete_profile(self):
         self.delete()
 
-   @receiver(post_save, sender=User)
+    @receiver(post_save, sender=User)
     def update_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
@@ -62,25 +62,25 @@ class Profile(models.Model):
 
 class Image(models.Model):
     pic = models.ImageField(upload_to='images/', blank=True)
-    caption = HTMLField()
+    caption = models.CharField(max_length=60, null=True)
     likes = models.ManyToManyField(User, related_name='likes', blank=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True)
 
-   def save_image(self):
+    def save_image(self):
         self.save()
 
-   def delete_image(self):
+    def delete_image(self):
         self.delete()
 
-   def __str__(self):
+    def __str__(self):
         return self.caption
 
-   @classmethod
+    @classmethod
     def get_images(cls):
         images = cls.objects.all()
         return images
 
-   def total_likes(self):
+    def total_likes(self):
         return self.likes.count()
 
 
@@ -89,5 +89,5 @@ class Comment(models.Model):
     image = models.ForeignKey(Image, null=True)
     user = models.ForeignKey(User, null=True)
 
-   def __str__(self):
-        return self.comment
+    def __str__(self):
+            return self.comment
--- insta/urls.py
+++ insta/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
@@ -8,3 +9,6 @@ urlpatterns=[
     url(r'^profile/$',views.profile,name='profile'),
     url(r'^explore/$',views.explore,name='explore'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
